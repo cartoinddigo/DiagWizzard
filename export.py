a49bf41eae42f82be6eeaa6acc2463d54453ed4f
cartoinddigo <34676078+cartoinddigo@users.noreply.github.com>
2018-02-24 20:56:59 +0100

    fix bug sur différentes échelles

--- export.py
+++ export.py
@@ -9,22 +9,26 @@ iface.newProject()
 pathuser = str(QFileDialog.getExistingDirectory(None, "Select Directory"))
 filter = "qgs(*.qgs)"
 path = pathuser
-project = QFileDialog.getOpenFileName(None, "Selectionner le fichier source", path+'/projects', filter)
-canvas = QgsMapCanvas(None)
-# Load project
-bridge = QgsLayerTreeMapCanvasBridge(QgsProject.instance().layerTreeRoot(), canvas)
-QgsProject.instance().read(QFileInfo(project))
-base = QFileInfo(project)
-nom = base.baseName()
-print (nom)
-# Load the composer
-composerView = qgis.utils.iface.activeComposers()[0]
-composition = composerView.composition()
-atlas = composition.atlasComposition()
-composition.setAtlasMode(QgsComposition.ExportAtlas)
-atlas.beginRender()
-for i in range(0,atlas.numFeatures()):
-    atlas.prepareForFeature(i)
-    image = composition.printPageAsRaster(i)
-    image.save(pathuser+'/export/'+nom+'-'+str(i)+'.png','png')
-atlas.endRender()
+listeqgs = os.listdir(pathuser+'/projects')
+#canvas = QgsMapCanvas(None)
+#bridge = QgsLayerTreeMapCanvasBridge(QgsProject.instance().layerTreeRoot(), canvas)
+for j in listeqgs:
+    project = pathuser+'/projects/'+str(j)
+    
+    
+    QgsProject.instance().read(QFileInfo(project))
+    base = QFileInfo(project)
+    nom = base.baseName()
+    composerView = qgis.utils.iface.activeComposers()[0]
+    composition = composerView.composition()
+    atlas = composition.atlasComposition()
+    composition.setAtlasMode(QgsComposition.ExportAtlas)
+    atlas.beginRender()
+    for i in range(0,atlas.numFeatures()):
+        atlas.prepareForFeature(i)
+        image = composition.printPageAsRaster(0)
+        image.save(pathuser+'/export/'+nom+'-Z'+str(i)+'.png','png')
+        
+    atlas.endRender()
+    print (nom+" exporte")
+    iface.newProject()
